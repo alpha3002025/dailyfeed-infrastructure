0f9a325e3e5a1fbbc1bc7bfcb6344718741785fb
alpha3002025 <alpha300uk@gmail.com>
2025-10-09 11:00:10 +0900

    chore: (helm) dev install script 정리

--- helm/1-install-essential-dev.sh
+++ helm/1-install-essential-dev.sh
@@ -1,15 +1,44 @@
+echo "🖨️🖨️🖨️  create namespace 'dailyfeed'"
 ## configmap, secret
 kubectl create ns dailyfeed
+echo ""
+
+
 ## infra
+echo " 🖨️🖨️🖨️  create namespace 'infra'"
 kubectl create ns infra
+echo ""
+
+
 ## install configmap, secret
-#kubectl apply -f manifests/dev/dev-config-secret.yaml
+echo " 🔑🔑🔑 create configmaps, secrets"
 cd manifests/dev
 kubectl apply -f .
 cd ../..
-## install kafka, redis
+echo ""
+
+
+## install kafka, redis, mongodb
+echo " 📦📦📦 install kafka, redis, mysql"
 cd kafka_redis_mysql
 source dev-setup.sh
 cd ..
-## install mysql
+echo ""
+
+## networking
+echo " 🛜🛜🛜 install services"
 kubectl apply -n infra -f kafka_redis_mysql/dev-mysql-service.yaml
+kubectl apply -n infra -f kafka_redis_mysql/dev-redis-service.yaml
+## ExternerService 로 대체 예정
+# kubectl apply -n infra -f kafka_redis_mysql/dev-mongodb-service.yaml
+
+#echo "wait 60s (mysql pending) "
+#sleep 60
+#
+### fort-forwarding
+#echo "port-forward -n infra svc/mysql 3306:3306 &"
+#kubectl port-forward -n infra svc/mysql 3306:3306 &
+
+echo "⛴️ create namespace 'dailyfeed' & istio-injection=enabled"
+kubectl create namespace dailyfeed
+kubectl label namespace istio-injection=enabled
