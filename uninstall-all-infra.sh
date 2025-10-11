c85dab0451ebf9252455d7dfe77f62e8af53a1a2
alpha3002025 <alpha300uk@gmail.com>
2025-10-11 13:43:06 +0900

    feat: Prometheus port 추가

--- uninstall-all-infra.sh
+++ uninstall-all-infra.sh
@@ -1,7 +1,11 @@
 echo "🛺🛺🛺  uninstall mysql,redis,kafka,mongodb with helm ... "
 cd helm
 source 1-uninstall-essential-local.sh
+cd ..
 echo ""
 
 ## uninstall kind
-
+echo "🛺🛺🛺  delete cluster"
+cd kind
+source delete-cluster.sh
+cd ..
