73bbc44096ad8c8c4d52115ea1a588a639bc8e2c
alpha3002025 <alpha300uk@gmail.com>
2025-10-12 20:23:33 +0900

    feat: redis 비밀번호 지정

--- helm/kafka_redis_mysql/install-redis.sh
+++ helm/kafka_redis_mysql/install-redis.sh
@@ -1,1 +1,3 @@
-helm install redis oci://registry-1.docker.io/cloudpirates/redis --namespace infra
+helm install redis oci://registry-1.docker.io/cloudpirates/redis \
+    --namespace infra \
+    --set auth.password=QmHCVICmJ63L6FIR
